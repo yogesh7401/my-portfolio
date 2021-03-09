b67fbff6d803ba2622928e45930bbcb8b5d2a1db
yogesh7401 <yogeshwaran7401@gmail.com>
2021-03-09 22:02:06 +0530

    Updates

---
+++ precache-manifest.40494f9a42dc986222856f98f9b93506.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05165fdb91319382d2bc94f0f97ef62",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "1b627f4ad7737bb33ad3",
+    "url": "/my-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c90f63ee3495d821b331",
+    "url": "/my-portfolio/static/css/main.5fa97b48.chunk.css"
+  },
+  {
+    "revision": "1b627f4ad7737bb33ad3",
+    "url": "/my-portfolio/static/js/2.9ba52573.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-portfolio/static/js/2.9ba52573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c90f63ee3495d821b331",
+    "url": "/my-portfolio/static/js/main.49d23a06.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "3aaf28c252f00012e41f2833557a5f6c",
+    "url": "/my-portfolio/static/media/Calculator.3aaf28c2.PNG"
+  },
+  {
+    "revision": "6e46a37a349f7d22ea56b530e61b3b29",
+    "url": "/my-portfolio/static/media/Face.6e46a37a.PNG"
+  },
+  {
+    "revision": "76095b270720a3bdc46d3b883d420219",
+    "url": "/my-portfolio/static/media/LWM.76095b27.PNG"
+  },
+  {
+    "revision": "5447cbb0f6f13871c6181106a6fab0b7",
+    "url": "/my-portfolio/static/media/Portfolio.5447cbb0.PNG"
+  },
+  {
+    "revision": "4f20a17395ec89a83e9ca6833755e451",
+    "url": "/my-portfolio/static/media/background.4f20a173.jpg"
+  },
+  {
+    "revision": "5818c3cb862d7f6043fe4448abc6ab44",
+    "url": "/my-portfolio/static/media/bot.5818c3cb.png"
+  },
+  {
+    "revision": "0c3ac35e8dcfaccbc51ad82c64cf4a92",
+    "url": "/my-portfolio/static/media/down.0c3ac35e.svg"
+  },
+  {
+    "revision": "5849f4750ab45237d622c2fc3c49cecb",
+    "url": "/my-portfolio/static/media/loader2.5849f475.gif"
+  },
+  {
+    "revision": "a9343a6ff87a6bc7aa4978adb0d62ece",
+    "url": "/my-portfolio/static/media/noisy.a9343a6f.png"
+  },
+  {
+    "revision": "b54e2681349e06e7e6a1a578270c0eff",
+    "url": "/my-portfolio/static/media/quote-close.b54e2681.png"
+  },
+  {
+    "revision": "e7aa2f33e997934bee3b73ac90991230",
+    "url": "/my-portfolio/static/media/quote-open.e7aa2f33.png"
+  },
+  {
+    "revision": "cb83f8aab166481ff2b149e69858e244",
+    "url": "/my-portfolio/static/media/sghrm.cb83f8aa.jpg"
+  }
+]);
